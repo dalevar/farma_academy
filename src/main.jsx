80ffe810754078c446c6a495baaf0f72b35af871
TemanKamu <lutf8363@gmail.com>
2023-11-03 00:30:10 +0800

    Fix

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import LandingPage from './pages/landing_page/App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 
-import { ThemeProvider } from "@material-tailwind/react";
+// import { ThemeProvider } from "@material-tailwind/react";
 
 const router = createBrowserRouter([
   {
@@ -19,6 +19,6 @@ const router = createBrowserRouter([
 ])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
