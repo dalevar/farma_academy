8a20323ec1724b561a59477f08418413a0120d61
Saidil Mifdal <saidilmifdal15@gmail.com>
2023-11-03 20:18:33 +0800

    modified:   src/components/Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,113 @@
 const Footer = () => {
-    return (
-      <div>Footer</div>
-    )
-  }
-  
-  export default Footer
+  return (
+    Footer
+    // <footer className="bg-gray-900">
+    //   <div className="container mx-auto px-6 pt-10 pb-6">
+    //     <div className="flex flex-wrap">
+    //       <div className="w-full md:w-1/4 text-center md:text-left">
+    //         <h5 className="uppercase mb-6 font-bold">Links</h5>
+    //         <ul className="mb-4">
+    //           <li className="mt-2">
+    //             <a
+    //               href="#"
+    //               className="hover:underline text-gray-600 hover:text-orange-500"
+    //             >
+    //               FAQ
+    //             </a>
+    //           </li>
+    //           <li className="mt-2">
+    //             <a
+    //               href="#"
+    //               className="hover:underline text-gray-600 hover:text-orange-500"
+    //             >
+    //               Help
+    //             </a>
+    //           </li>
+    //           <li className="mt-2">
+    //             <a
+    //               href="#"
+    //               className="hover:underline text-gray-600 hover:text-orange-500"
+    //             >
+    //               Support
+    //             </a>
+    //           </li>
+    //         </ul>
+    //       </div>
+    //       <div className="w-full md:w-1/4 text-center md:text-left">
+    //         <h5 className="uppercase mb-6 font-bold">Legal</h5>
+    //         <ul className="mb-4">
+    //           <li className="mt-2">
+    //             <a
+    //               href="#"
+    //               className="hover:underline text-gray-600 hover:text-orange-500"
+    //             >
+    //               Terms
+    //             </a>
+    //           </li>
+    //           <li className="mt-2">
+    //             <a
+    //               href="#"
+    //               className="hover:underline text-gray-600 hover:text-orange-500"
+    //             >
+    //               Privacy
+    //             </a>
+    //           </li>
+    //         </ul>
+    //       </div>
+    //       <div className="w-full md:w-1/4 text-center md:text-left">
+    //         <h5 className="uppercase mb-6 font-bold">Social</h5>
+    //         <ul className="mb-4">
+    //           <li className="mt-2">
+    //             <a
+    //               href="#"
+    //               className="hover:underline text-gray-600 hover:text-orange-500"
+    //             >
+    //               Facebook
+    //             </a>
+    //           </li>
+    //           <li className="mt-2">
+    //             <a
+    //               href="#"
+    //               className="hover:underline text-gray-600 hover:text-orange-500"
+    //             >
+    //               LinkedIn
+    //             </a>
+    //           </li>
+    //         </ul>
+    //       </div>
+    //       <div className="w-full md:w-1/4 text-center md:text-left">
+    //         <h5 className="uppercase mb-6 font-bold">Company</h5>
+    //         <ul className="mb-4">
+    //           <li className="mt-2">
+    //             <a
+    //               href="#"
+    //               className="hover:underline text-gray-600 hover:text-orange-500"
+    //             >
+    //               Official Blog
+    //             </a>
+    //           </li>
+    //           <li className="mt-2">
+    //             <a
+    //               href="#"
+    //               className="hover:underline text-gray-600 hover:text-orange-500"
+    //             >
+    //               About Us
+    //             </a>
+    //           </li>
+    //         </ul>
+    //       </div>
+    //     </div>
+    //   </div>
+    //   <div className="container mx-auto px-6">
+    //     <div className="mt-16 border-t-2 border-gray-300 flex flex-col items-center">
+    //       <div className="sm:w-2/3 text-center py-6">
+    //         <p className="text-sm text-gray-600 mb-2">
+    //           © 2020 by Farma Academy. All rights reserved.
+    //         </p>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </footer>
+  );
+};
+export default Footer;
