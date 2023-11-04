8cbe7bc162172064f9d37a262424287841783661
Ramadhan5 <90996720+Ramadhan5@users.noreply.github.com>
2023-11-04 21:27:31 +0800

    deleted:    public/images/BIG LOGO.png 	deleted:    public/images/Main Logo.png 	new file:   public/images/calculator.png 	deleted:    public/images/logo1.png 	new file:   public/logos/BigLogo.png 	renamed:    public/images/Logo.png -> public/logos/Logo.png 	renamed:    public/images/Text - Logo.png -> public/logos/TextLogo.png 	new file:   src/components/Dropdown.jsx 	new file:   src/components/Icons.jsx 	new file:   src/components/card/Calculator.jsx 	modified:   src/components/layouts/Footer.jsx 	modified:   src/components/layouts/Header.jsx 	modified:   src/pages/landing_page/App.jsx 	modified:   tailwind.config.js

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,7 +1,19 @@
 export default function Footer() {
     return (
         <footer>
-            footer
+            <div className="container mx-auto flex text-farma-950">
+                <div className="basis-[30%]">
+                    <div className="w-full">
+                        <img src="/logos/BigLogo.png" alt="" />
+                    </div>
+                    <div>
+                        <p className="break-words leading-tight text-lg w-full text-center">Academy Online Farmasi pertama yang telah berkerjasama dengan DUDI</p>
+                    </div>
+                </div>
+                <div className="">
+
+                </div>
+            </div>
         </footer>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,22 +3,27 @@
 export default ({
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    colors: { 
-      "white" : "#FFFFFF",
-      "farma-50" : "#EFFAFF",
-      "farma-100" : "#DFF4FF",
-      "farma-200" : "#B8EBFF",
-      "farma-300" : "#78DDFF",
-      "farma-400" : "#33CCFF",
-      "farma-500" : "#06B5F1",
-      "farma-600" : "#0092CE",
-      "farma-700" : "#0075A7",
-      "farma-800" : "#02628A",
-      "farma-900" : "#085172",
-      "farma-950" : "#06334B"
-    },
-    fonts: {
-      sans: ["Inter", "sans-serif"],
+    extend: {
+      colors: {
+        "white": "#FFFFFF",
+        "farma-50": "#EFFAFF",
+        "farma-100": "#DFF4FF",
+        "farma-200": "#B8EBFF",
+        "farma-300": "#78DDFF",
+        "farma-400": "#33CCFF",
+        "farma-500": "#06B5F1",
+        "farma-600": "#0092CE",
+        "farma-700": "#0075A7",
+        "farma-800": "#02628A",
+        "farma-900": "#085172",
+        "farma-950": "#06334B"
+      },
+      fonts: {
+        sans: ["Inter", "sans-serif"],
+      },
+      boxShadow: {
+        calculatorCardShadow: '2px 4px 6px -1px #085172',
+      },
     },
   },
   plugins: [],
