5d010dae05323606284917237a77513ebb2d7b3b
Ramadhan5 <90996720+Ramadhan5@users.noreply.github.com>
2023-11-04 17:26:02 +0800

    deleted:    public/images/Logo - Small.png 	new file:   public/images/Logo.png 	renamed:    src/components/Footer.jsx -> src/components/_Footer.jsx 	renamed:    src/components/Navbar.jsx -> src/components/_Navbar.jsx 	new file:   src/components/layouts/Footer.jsx 	new file:   src/components/layouts/Header.jsx 	modified:   src/main.jsx 	modified:   src/pages/landing_page/App.jsx 	deleted:    src/pages/landing_page/Layout.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Dashboard from './pages/dashboard/App.jsx'
-import LandingPage from './pages/landing_page/App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+
+
+import Header from './components/layouts/Header.jsx'
+import Footer from './components/layouts/Footer.jsx'
+
+import Dashboard from './pages/dashboard/App.jsx'
+import LandingPage from './pages/landing_page/App.jsx'
 
 // import { ThemeProvider } from "@material-tailwind/react";
 
@@ -20,7 +25,13 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <ThemeProvider> */}
-    <RouterProvider router={router} />
+    <div className='flex flex-col min-h-screen'>
+      <Header />
+      <main className='flex-auto'>
+        <RouterProvider router={router} />
+      </main>
+      <Footer />
+    </div>
     {/* </ThemeProvider> */}
   </React.StrictMode>,
 )
