ce70e9f8ec3ed79a745c194b7e7baf242e9b5c60
Saidil Mifdal <saidilmifdal15@gmail.com>
2023-11-06 14:04:16 +0800

    modified:   src/App.jsx 	new file:   src/components/cards/Card.Module.jsx 	new file:   src/pages/accsess_module_page/App.jsx 	new file:   src/pages/accsess_module_page/BodyAccsessSection.jsx 	new file:   src/pages/accsess_module_page/HeaderAccsessSection.jsx 	modified:   src/pages/landing_page/App.jsx 	modified:   src/pages/landing_page/BannerSection.jsx 	new file:   src/pages/landing_page/CTA.jsx 	modified:   src/pages/landing_page/TrustedBySection.jsx 	new file:   src/pages/module_page/App.jsx 	new file:   src/pages/module_page/BodyModuleSection.jsx 	new file:   src/pages/module_page/HeaderModuleSection.jsx

--- src/pages/landing_page/TrustedBySection.jsx
+++ src/pages/landing_page/TrustedBySection.jsx
@@ -1,16 +1,18 @@
 import CardTrustedBy from "../../components/cards/CardTrustedBy";
 
 export default function TrustedBySection() {
-    return (
-        <section className="mt-60">
-            <h2 className="text-center break-words text-farma-950 md:text-4xl text-2xl font-semibold leading-tight tracking-tight">Telah diPercayai Oleh</h2>
+  return (
+    <section className="mt-[5rem]">
+      <h2 className="text-2xl font-semibold leading-tight tracking-tight text-center break-words text-farma-950 md:text-4xl">
+        Telah dipercayai Oleh
+      </h2>
 
-            <div className="flex gap-4 justify-center mt-10">
-                <CardTrustedBy />
-                <CardTrustedBy />
-                <CardTrustedBy />
-                <CardTrustedBy />
-            </div>
-        </section>
-    )
+      <div className="flex justify-center gap-4 mt-10">
+        <CardTrustedBy />
+        <CardTrustedBy />
+        <CardTrustedBy />
+        <CardTrustedBy />
+      </div>
+    </section>
+  );
 }
