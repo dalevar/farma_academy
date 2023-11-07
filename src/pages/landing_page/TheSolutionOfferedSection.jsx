e90911511f336081594bae934826d8471d9be627
Ramadhan5 <90996720+Ramadhan5@users.noreply.github.com>
2023-11-07 22:50:50 +0800

    modified:   src/pages/landing_page/TheSolutionOfferedSection.jsx

--- src/pages/landing_page/TheSolutionOfferedSection.jsx
+++ src/pages/landing_page/TheSolutionOfferedSection.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function TheSolutionOffered() {
     return (
         <section>
-            <div className="flex justify-between container mx-auto border-b-2">
+            <div className="flex justify-between container mx-auto border-b-2 py-10">
                 <p className="">Solusi belajar farmasi yang terjangkau dan efektif</p>
                 <Link className="py-2 px-3 bg-farma-700 text-white rounded-md">Daftar Sekarang</Link>
             </div>
