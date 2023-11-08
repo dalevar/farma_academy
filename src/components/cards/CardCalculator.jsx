36b69d456714eab9e8f0234d39a3bfb294207137
Ramadhan5 <90996720+Ramadhan5@users.noreply.github.com>
2023-11-09 00:33:41 +0800

    modified:   src/components/Icons.jsx 	modified:   src/components/cards/CardCalculator.jsx 	modified:   src/pages/landing_page/CalculatorSection.jsx

--- src/components/cards/CardCalculator.jsx
+++ src/components/cards/CardCalculator.jsx
@@ -1,15 +1,17 @@
 import PropTypes from 'prop-types';
+import { IconCalculator } from '../Icons';
+import { Link } from 'react-router-dom';
 
 export default function CardCalculator({ calculatorTitle, href }) {
     return (
-        <a href={href} className="flex flex-col items-center bg-farma-100 w-32 p-3 justify-center gap-4 rounded-md shadow-CardShadow">
-            <div>
-                <img src="/images/calculator.png" alt="" />
+        <Link to={href} className="border-2 rounded-lg border-farma-600 p-4 flex flex-col items-center">
+            <div className="w-10 h-10">
+                <IconCalculator />
             </div>
             <div>
-                <p className="text-center leading-snug">{calculatorTitle}</p>
+                <p className="w-32 text-center mt-5 text-sm text-farma-700">{calculatorTitle}</p>
             </div>
-        </a>
+        </Link>
     )
 }
 
--- src/pages/landing_page/CalculatorSection.jsx
+++ src/pages/landing_page/CalculatorSection.jsx
@@ -2,33 +2,40 @@
 import {
   IconDecorationRight,
   IconDecorationLeft,
+  IconArrowRight,
 } from "../../components/Icons";
+import CardCalculator from "../../components/cards/CardCalculator";
 export default function CalculatorSection() {
   return (
-    <section id="calculator" className="flex flex-col px-3 gap-5 w-full h-96 ">
-      <div className="relative">
-        <div className="absolute -left-3">
-          <IconDecorationLeft />
-        </div>
-      </div>
-      <div className="flex justify-center relative">
-        <div className="flex justify-center">
-          <p className="text-3xl text-farma-950 font-bold w-5/12 text-center">
-            Ingin menghitung timbangan obat dengan mudah dan cepat?
-          </p>
-        </div>
-          <span>
-            Gunakan perhitungan timbangan obat dari Farma Academy! Alat ini
-            dilengkapi dengan berbagai fitur yang dapat memudahkan Anda dalam
-            menghitung timbangan obat
-          </span>
+    <section id="calculator" className="flex flex-col px-3 gap-5 w-full h-96 relative justify-center items-center">
+
+      <div className="absolute -left-3">
+        <IconDecorationLeft />
       </div>
 
-      <div className="relative z-50">
-        <div className="absolute -right-2 ">
-          <IconDecorationRight />
+      <div className="">
+        <div>
+          <p className="break-words font-semibold text-4xl tracking-tight leading-tight text-center">Ingin menghitung timbangan obat dengan <br /> mudah dan cepat? </p>
+          <p className="break-words text-center tracking-wide text-xl text-farma-900 mt-4 mb-8">Gunakan perhitungan timbangan obat dari Farma Academy! <br />
+            Alat ini dilengkapi dengan berbagai fitur yang dapat memudahkan <br /> Anda dalam menghitung timbangan obat</p>
+        </div>
+        <div className="flex gap-5 items-center">
+          <CardCalculator calculatorTitle="Kalkulator timbangan obat" href="" />
+          <CardCalculator calculatorTitle="Kalkulator timbangan obat" href="" />
+          <CardCalculator calculatorTitle="Kalkulator timbangan obat" href="" />
+          <CardCalculator calculatorTitle="Kalkulator timbangan obat" href="" />
+          <a href="" className="w-6 h-6 bg-primary-dark rounded-full flex justify-center items-center">
+            <span className="w-2 h-2 inline-block">
+              <IconArrowRight />
+            </span>
+          </a>
         </div>
       </div>
+
+      <div className="absolute -right-2">
+        <IconDecorationRight />
+      </div>
+
     </section>
   );
 }
