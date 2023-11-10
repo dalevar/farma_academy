94ee7d1f82658b3ec422cefcb9e3cbe3cd3365fa
TemanKamu <lutf8363@gmail.com>
2023-11-11 02:16:38 +0800

    modified:   src/App.jsx 	new file:   src/pages/module_page/quiz_interaktif_module/App.jsx 	modified:   package-lock.json 	modified:   package.json 	modified:   src/pages/module_page/quiz_interaktif_module/App.jsx

---
+++ src/pages/module_page/quiz_interaktif_module/App.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import { IconCheckedV2 } from "../../../components/Icons";
+const QuizInteraktif = () => {
+  return (
+    <div className="flex m-5">
+     
+      <div className="w-full border rounded-xl mx-3  p-5">
+        <div>
+            Latihan
+            <div className="font-bold">
+                Hitunglah Paracetamol untuk anak berusia 18 tahun
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default QuizInteraktif;
