0d6f21801c083d2be8257469d339beea25d0e096
TemanKamu <lutf8363@gmail.com>
2023-11-11 02:41:50 +0800

    Update fikx bug

--- src/components/card_kalkulator/CardYoung.jsx
+++ src/components/card_kalkulator/CardYoung.jsx
@@ -23,22 +23,22 @@ const CardYoung = () => {
     dm = dm.replace(/[^0-9.]/g, "");
     dm = Number(dm);
 
-    if(e.target[1].value === "Umur anak"){
-      return alert("Masukkan umur!")
+    if (e.target[1].value === "Umur anak") {
+      return alert("Masukkan umur!");
     }
     if (e.target[2].value) {
       let dmSatuObat = e.target[2].value.toString();
       dmSatuObat = dmSatuObat.replace(",", ".");
       dmSatuObat = dmSatuObat.replace(/[^0-9.]/g, "");
       dmSatuObat = Number(dmSatuObat);
-      const resultsSatu = ((n / 20) * dmSatuObat).toFixed(3);
+      const resultsSatu = ((n / (n + 12)) * dmSatuObat).toFixed(3);
       setResultSatuObat(resultsSatu);
-    }else{
-        setResultSatuObat(null)
+    } else {
+      setResultSatuObat(null);
     }
     setUmur(n);
     setRumus(`\\frac{n}{{n+12}} \\times dm`);
-    setMathExpression(`\\frac{${n}}{{${n}+12}} \\times ${dm}g`);
+    setMathExpression(`\\frac{${n}}{{${n}+12}} \\times ${dm}mg`);
     const results = ((n / (n + 12)) * dm).toFixed(3);
     setResult(results);
   }
@@ -64,7 +64,9 @@ const CardYoung = () => {
                   className="peer border-2 border-gray-400 rounded-lg w-full py-4 px-2
                     outline-none"
                 >
-                  <option selected disabled>Umur anak</option>
+                  <option selected disabled>
+                    Umur anak
+                  </option>
                   {catatan.umurTahun.map((v, i) => {
                     return (
                       <option value={v} key={i}>
@@ -137,10 +139,12 @@ const CardYoung = () => {
                 DM 1 Hari yang harus diberikan kepada anak usia {umur} tahun
                 adalah <span className="font-bold">{result} mg </span>
               </span>
-              <span className="text-sm block">
-                DM 1x yang harus diberikan kepada anak usia {umur} tahun
-                adalah <span className="font-bold">{resultSatuObat} mg </span>
-              </span>
+              {resultSatuObat && (
+                <span className="text-sm block">
+                  DM 1x yang harus diberikan kepada anak usia {umur} tahun
+                  adalah <span className="font-bold">{resultSatuObat} mg </span>
+                </span>
+              )}
             </div>
           </div>
         )}
